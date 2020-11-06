e99d80959d50cadaf5788b0f07384d47d955070f
sinho <dltlsgh5@gmail.com>
2020-11-06 15:33:16 +0900

    클래스형 컴포넌트를 이용한 state 관리

--- src/Counter.js
+++ src/Counter.js
@@ -1,27 +1,47 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cnt: 0,
-      num: this.props.num,
-    };
-  }
-
+  //   state사용하는 첫번째 방법
+  //     constructor(props) {
+  //     super(props);
+  //     this.state = {
+  //       cnt: 0,
+  //       num: this.props.num,
+  //     };
+  //   }
+  // state를 사용하는 두번째 방법
+  state = {
+    cnt: 0,
+    num: this.props.num,
+  };
   render() {
     const { cnt, num } = this.state;
+    const { props_num } = this.props.num;
+
+    const onClickHandler = (e) => {
+      console.log(e);
+      //   this.setState({ num: num + 1 });
+      //   this.setState((ps) => {
+      //     return { num: ps.num + 1 };
+      //   });
+      //   this.setState({ cnt: cnt + 1 });
+      //   this.setState({ num: num + 1 });
+      this.setState(
+        (ps) => ({ cnt: ps.cnt + 1 }),
+        () => {
+          console.log("변하기 전cnt: ", cnt);
+          console.log("변한 후 cnt: ", this.state.cnt);
+        }
+      );
+      //   this.setState((ps) => ({
+      //     num: ps.num + 1,
+      //   }));
+    };
     return (
       <>
         <div>{cnt}</div>
         <div>Counter에서의 num {num}</div>
-        <button
-          onClick={() => {
-            this.setState({ num: num + 1 });
-          }}
-        >
-          +1
-        </button>
+        <button onClick={onClickHandler}>+1</button>
       </>
     );
   }
