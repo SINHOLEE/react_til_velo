388bfab97fc4197fe9a849e8c90b5d7f1b7246d1
sinho <dltlsgh5@gmail.com>
2020-11-16 10:24:21 +0900

    refactor: scss

--- styling-react/src/SassComponent.js
+++ styling-react/src/SassComponent.js
@@ -1,30 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState, useRef} from "react";
 import "./SassComponent.scss";
 
 const SassComponent = () => {
-  const onClick = (e) => {
-    console.log(e);
-  };
-  useEffect(() => {
-    let cnt = 0;
-    setInterval(() => {
-      cnt += 1;
-      console.log(cnt);
-    }, 2000);
-    // setInterval(() => {
-    //   console.log(cnt);
-    // }, 2000);
-  }, []);
-  console.log("rendered");
-  return (
-    <div className="SassComponent" onClick={onClick}>
-      <div className="box red"></div>
-      <div className="box orange"></div>
-      <div className="box yellow"></div>
-      <div className="box green"></div>
-      <div className="box blue"></div>
-    </div>
-  );
+  const colorList = ["red", "orange", "yellow", "green", "blue"];
+
+  const [Elements, setElements] = useState(colorList.map((color, idx) => <div className={"box " + color} key={idx}></div>));
+  return <div className="SassComponent">{Elements}</div>;
 };
 
 export default SassComponent;
